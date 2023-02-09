7e57a7fab63007bfc2467428daa8f329924c4028
koushikpamulapati <124774794+koushikpamulapati@users.noreply.github.com>
2023-02-09 14:40:11 +0530

    day 2

---
+++ day 2 C-4.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+  int a,b;
+  printf("enter a,b values");
+  scanf("%d%d",&a,&b);
+  
+  if (a>b)
+  printf("a is greater than b");
+  else
+  printf("a is less than b");
+  return 0;
+}
